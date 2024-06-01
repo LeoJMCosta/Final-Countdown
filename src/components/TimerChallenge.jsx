b7d73f5248c34183e36f43b44dbca9530285a42f
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-06-01 19:31:58 +0100

    timeout

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
-    return <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-            {targetTime} second{targetTime > 1 ? 's' : ''}
-        </p>
-        <p>
-            <button>
-                Start Challenge
-            </button>
-        </p>
-        <p className="">
-            Time is running... /Timer inactive
-        </p>
+  const [timerExpired, setTimerExperired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExperired(true);
+    }, targetTime * 1000);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Time is running... /Timer inactive</p>
     </section>
+  );
 }
